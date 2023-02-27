15b738e684d4985c3a4d488f376a2d6b7f690809
fatima lahnoud <fatimalahnoud2001@gmail.com>
2023-02-27 03:33:52 +0100

    rapport_upload

--- app/src/main/java/com/example/searchprojet/ArchiveActivity.java
+++ app/src/main/java/com/example/searchprojet/ArchiveActivity.java
@@ -1,14 +1,42 @@
 package com.example.searchprojet;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ArchiveActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class ArchiveActivity extends AppCompatActivity {
+    TextView txtMesRapprt,txtTousRapprt;
+    FloatingActionButton Ajouter;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archive);
+        txtMesRapprt=findViewById(R.id.MesArchives);
+        txtTousRapprt=findViewById(R.id.TousArchive);
+        Ajouter=findViewById(R.id.ajouterRapport);
+
+        Ajouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4=new Intent(ArchiveActivity.this,UploadRapport.class);
+                startActivity(intent4);
+            }
+        });
+
+        txtTousRapprt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ArchiveActivity.this, TousRapport.class);
+                startActivity(intent);
+            }
+        });
     }
 }
