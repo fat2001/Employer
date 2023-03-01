41351e413a1e080cf86077a336764b639df2c3ef
fatima lahnoud <fatimalahnoud2001@gmail.com>
2023-03-01 12:34:58 +0100

    update

--- app/src/main/java/com/example/searchprojet/ResponsableAnnonces.java
+++ app/src/main/java/com/example/searchprojet/ResponsableAnnonces.java
@@ -1,6 +1,8 @@
 package com.example.searchprojet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -12,7 +14,11 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class ResponsableAnnonces extends AppCompatActivity {
+    static RecyclerView recyclerView;
+    static Anonce_adapter adapter;
  TextView txtMesAnnonces;
  TextView txtTousAnnonces;
  LinearLayout ln;
@@ -25,6 +31,19 @@ public class ResponsableAnnonces extends AppCompatActivity {
         txtMesAnnonces = findViewById(R.id.MesAnnonces);
         txtTousAnnonces = findViewById(R.id.TousAnnonces);
         Ajouter=findViewById(R.id.ajouter);
+
+        Connecxion con = new Connecxion();
+
+        recyclerView = (RecyclerView) findViewById(R.id.receyclerannonce);
+        ArrayList<Activite> list = new ArrayList<>();
+        // list.add(new Activite("22/4/2023", "لنحترم قانون السير", "Casablanca"));
+        //  list.add(new Activite("26/09/2023", "يجب استخدام كاميرات المراقبة من طرف الشرطة والدرك لتفادي الرشوة هكذا من تبثث في حقه مخالفة سيؤديها ومن حاول ارشاء الشرطة او الدرك سيحرر في حقه محضر الارتشاء", "Guelmim"));
+        //  list.add(new Activite("03/12/2023", "وتنظم بالمؤسسات التعليمية بهذه المناسبة، أنشطة تربوية وفنية وثقافية ومسابقات، اعتبارا لأهمية توعية المتعلمات والمتعلمين بقواعد السلامة الطرقية، وترسيخ ثقافة احترامها لدى الناشئة، من أجل حياة آمنة خالية من المخاطر.", "Tiznit"));
+        //Anonce_adapter adapter = new Anonce_adapter(, this);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1, RecyclerView.VERTICAL,false));
+        // recyclerView.setAdapter(adapter);
+        con.getallactivity(this);
+        // con.getmesactivity(1,this);
 
         txtTousAnnonces.setOnClickListener(new View.OnClickListener() {
             @Override
